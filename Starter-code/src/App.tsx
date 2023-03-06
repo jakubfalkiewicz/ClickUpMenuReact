247d9d4384bd794ce18ba61502201875e4db78cb
Jakub Falkiewicz <kufal222@gmail.com>
2023-03-06 23:29:45 +0100

    initial commit

---
+++ Starter-code/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HomePage } from "@modules";
+import './App.scss'
+
+const App: React.FC = () => (
+  <>
+    <HomePage />
+  </>
+);
+
+export default App;
---
+++ Starter-code/src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./HomePage.scss";
+
+
+
+export const HomePage = () => {
+
+
+  return (
+    <div className="homepage">
+      <nav>
+        <div className='navbar-container'>
+          <div className='navbar-left'>
+            <div className='logo'></div>
+            <div className='tabs-container'>
+              <div className='tab'></div>
+              <div className='tab'></div>
+              <div className='tab'></div>
+              <div className='tab'></div>
+              <div className='tab'></div>
+            </div>
+          </div>
+          <div className='navbar-right'></div>
+        </div>
+      </nav>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+};
