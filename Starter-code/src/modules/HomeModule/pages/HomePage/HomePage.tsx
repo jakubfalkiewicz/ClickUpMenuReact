570c10e75046cef2f343498acb7781ed80fa2692
Jakub Falkiewicz <kufal222@gmail.com>
2023-03-07 02:40:39 +0100

    nav-bar

--- Starter-code/src/modules/HomeModule/pages/HomePage/HomePage.tsx
+++ Starter-code/src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomePage.scss";
-
+import { RiArrowDownSLine } from "react-icons/ri";
 
 
 export const HomePage = () => {
@@ -11,16 +11,22 @@ export const HomePage = () => {
       <nav>
         <div className='navbar-container'>
           <div className='navbar-left'>
-            <div className='logo'></div>
+            <div className='logo'>ClickUp</div>
             <div className='tabs-container'>
-              <div className='tab'></div>
-              <div className='tab'></div>
-              <div className='tab'></div>
-              <div className='tab'></div>
-              <div className='tab'></div>
+              <div className='tab'><div>Product</div><RiArrowDownSLine size={20} /></div>
+              <div className='tab'><div>Solutions</div><RiArrowDownSLine size={20} /></div>
+              <div className='tab'><div>Learn</div><RiArrowDownSLine size={20} /></div>
+              <div className='tab'><div>Pricing</div></div>
+              <div className='tab'><div>Enterprise</div></div>
             </div>
           </div>
-          <div className='navbar-right'></div>
+          <div className='navbar-right'>
+            <div className="contact">Contact Sales</div>
+            <div className="sign-buttons">
+              <button className="sign-up-button">Sign Up</button>
+              <button className="log-in-button">Log In</button>
+            </div>
+          </div>
         </div>
       </nav>
       <main>
